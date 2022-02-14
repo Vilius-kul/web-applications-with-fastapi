181dbf84743b2e867a2018d8a3db7eaa777419ec
vilius <vilius.kulinas@gmail.com>
2022-02-14 15:56:03 +0000

    basic FastAPI set up

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello Fast!")
+from importlib import reload
+
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+def index():
+    return "Hello Fast!!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
