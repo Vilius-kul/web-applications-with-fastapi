dfda28ca3aef33632e3d624ca1ecc6ad4cd03eba
vilius <vilius.kulinas@gmail.com>
2022-02-17 12:16:39 +0000

    Users and HTML forms

--- views/account.py
+++ views/account.py
@@ -1,7 +1,9 @@
 import fastapi
 from fastapi_chameleon import template
+from infrastructure import cookie_auth
+from services import user_service
+from starlette import status
 from starlette.requests import Request
-
 from viewmodels.account.account_viewmodel import AccountViewModel
 from viewmodels.account.login_viewmodel import LoginViewModel
 from viewmodels.account.register_viewmodel import RegisterViewModel
@@ -9,24 +11,79 @@ from viewmodels.account.register_viewmodel import RegisterViewModel
 router = fastapi.APIRouter()
 
 
-@router.get('/account')
+@router.get("/account")  # type: ignore
+@template()
 def index(request: Request):
     vm = AccountViewModel(request)
     return vm.to_dict()
 
 
-@router.get('/account/register')
-def register(request: Request):
+@router.get("/account/register")  # type: ignore
+@template()
+def register(request: Request):  # type: ignore
     vm = RegisterViewModel(request)
     return vm.to_dict()
 
 
-@router.get('/account/login')
-def login(request: Request):
+@router.post("/account/register")  # type: ignore
+@template()
+async def register(request: Request):
+    vm = RegisterViewModel(request)
+    await vm.load()
+
+    if vm.error:
+        return vm.to_dict()
+
+    # Create the account
+    account = user_service.create_account(vm.name, vm.email, vm.password)  # type: ignore
+
+    # Login user
+    response = fastapi.responses.RedirectResponse(
+        url="/account", status_code=status.HTTP_302_FOUND
+    )
+    cookie_auth.set_auth(response, account.id)
+
+    return response
+
+
+# ################### LOGIN #################################
+
+
+@router.get("/account/login")  # type: ignore
+@template(template_file="account/login.pt")
+def login_get(request: Request):
     vm = LoginViewModel(request)
     return vm.to_dict()
 
 
-@router.get('/account/logout')
-def logout(request: Request):
-    return {}
+@router.post("/account/login")  # type: ignore
+@template(template_file="account/login.pt")
+async def login_post(request: Request):
+
+    vm = LoginViewModel(request)
+    await vm.load()
+
+    if vm.error:
+        return vm.to_dict()
+
+    user = user_service.login_user(vm.email, vm.password)
+    if not user:
+        vm.error = "The account does not exist or the password is wrong."
+        return vm.to_dict()
+
+    resp = fastapi.responses.RedirectResponse(
+        "/account", status_code=status.HTTP_302_FOUND
+    )
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
+
+
+@router.get("/account/logout")
+def logout():
+    response = fastapi.responses.RedirectResponse(
+        url="/", status_code=status.HTTP_302_FOUND
+    )
+    cookie_auth.logout(response)
+
+    return response
